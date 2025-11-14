f71ae79695f32104900efa4fb8d898d28d8ac217
Fredrik Kron <fkron95@gmail.com>
2025-11-14 11:55:03 +0100

    melt csv to long table and some gitignore

--- create_ingest_db/constants.py
+++ create_ingest_db/constants.py
@@ -7,6 +7,6 @@ con = duckdb.connect("database/näringsportalen.duckdb")
 
 # Path to csv and table name for staging schema in duckdb
 
-data_path = Path(__file__).parents[1] / "data" / "csv_data_livsmedelsverket.csv"
+data_path = Path(__file__).parents[1] / "data" / "csv_data_livsmedelsverket_melted.csv"
 
 table_name = "data_Livsmedelsverket"
