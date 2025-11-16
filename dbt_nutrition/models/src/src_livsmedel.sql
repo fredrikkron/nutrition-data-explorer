ad75f2999b9d86aed988e8c5e6fbd62dd7b80223
Fredrik Kron <fkron95@gmail.com>
2025-11-16 16:49:05 +0100

    src dbt testing

--- dbt_nutrition/models/src/src_livsmedel.sql
+++ dbt_nutrition/models/src/src_livsmedel.sql
@@ -3,10 +3,10 @@ with stg_livsmedel as (select * from {{ source('staging', 'data_Livsmedelsverket
 select
     Livsmedelsnummer as food_id,
     Livsmedelsnamn as food_name,
-    Gruppering as food_group,
+    coalesce(Gruppering, 'Övrigt') as food_group,
     trim(split_part(Näringsämne, ' (', 1)) as nutrient_name,
     trim(replace(replace(split_part(Näringsämne, '(', 2), ')', ''), ' ', '')) as unit,
     Värde as value
 from
     stg_livsmedel
-where unit != 'kJ'
+where unit NOT IN ('kJ', 'C22:5', 'C20:5', 'C22:6', 'NE/mg', 'skaletc.', 'RE/µg')
