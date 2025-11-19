65bede190f5f4e15ab599358b11ba50186c6ad2b
Fredrik Kron <fkron95@gmail.com>
2025-11-19 16:18:29 +0100

    marts for KPI

---
+++ dbt_nutrition/models/mart/KPI_Protein.sql
@@ -0,0 +1,32 @@
+with KPIs AS (
+SELECT
+    f.food_id,
+    f.food_name,
+    n.nutrient_name,
+    fn.value
+    from {{ ref('fct_food_nutrients')}} fn
+INNER JOIN {{ ref('dim_food')}} f
+    ON fn.food_id = f.food_id
+INNER JOIN {{ ref('dim_nutrients')}} n
+    ON fn.nutrient_id = n.nutrient_id
+WHERE n.nutrient_name IN ('Protein')
+),
+
+pivot_data AS (
+SELECT
+    food_id,
+    food_name,
+    MAX(value) AS protein_value
+FROM KPIs
+GROUP BY
+    food_id,
+    food_name
+)
+
+SELECT
+    food_id,
+    food_name,
+    protein_value
+FROM pivot_data
+WHERE protein_value > 10.0
+ORDER BY protein_value DESC
