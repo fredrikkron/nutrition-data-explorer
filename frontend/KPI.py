2989e85e02bc964b25e67f29a6b065226d1d397e
Fredrik Kron <fkron95@gmail.com>
2025-11-19 16:21:36 +0100

    start of dashboard and styling

---
+++ frontend/KPI.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import duckdb
+
+con = duckdb.connect("database/näringsportalen.duckdb")
+
+df_KPI_protein = con.execute("""
+    SELECT
+        *
+    FROM
+        marts.KPI_Protein
+    """).fetchdf()
+
+df_KPI_sugar = con.execute("""
+    SELECT
+        *
+    FROM
+        marts.KPI_Sugar
+    """).fetchdf()
+
+df_KPI_kcal = con.execute("""
+    SELECT
+        *
+    FROM
+        marts.KPI_Kcal
+    """).fetchdf()
+
+con.close()
