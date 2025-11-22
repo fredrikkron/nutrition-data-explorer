91db77c39bb0b8eec7ab859d29fd28dd64de4a24
Fredrik Kron <fkron95@gmail.com>
2025-11-22 16:45:53 +0100

    chart for dashboard

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -1,5 +1,14 @@
 import taipy.gui.builder as tgb
+from frontend.constants import connect_to_db
 from frontend.KPI import df_KPI_protein, df_KPI_sugar, df_KPI_kcal
+from frontend.charts import protein_chart
+
+con = connect_to_db()
+df_protein = con.execute("SELECT * FROM marts.chart_protein_comparison").fetchdf()
+con.close()
+
+# charts
+fig_protein = protein_chart()
 
 with tgb.Page() as dashboard_page:
     with tgb.part(class_name="container-card"):
@@ -17,3 +26,6 @@ with tgb.Page() as dashboard_page:
             with tgb.part(class_name="metric-card"):
                 tgb.text("### > 400 kcal", mode="md")
                 tgb.text(f"### {len(df_KPI_kcal)}", mode="md")
+        with tgb.part():
+            tgb.chart(figure="{fig_protein}")
+        
