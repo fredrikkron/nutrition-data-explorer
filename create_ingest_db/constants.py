b8750538d3031bc33b345229d3572e196bda6dc9
Fredrik Kron <fkron95@gmail.com>
2025-11-23 10:34:54 +0100

    updated data

--- create_ingest_db/constants.py
+++ create_ingest_db/constants.py
@@ -7,6 +7,6 @@ con = duckdb.connect("database/näringsportalen.duckdb")
 
 # Path to csv and table name for staging schema in duckdb
 
-data_path = Path(__file__).parents[1] / "data" / "csv_data_livsmedelsverket_melted.csv"
+data_path = Path(__file__).parents[1] / "data" / "csv_data_livsmedelsverket_melted_2025-10-29.csv"
 
 table_name = "data_Livsmedelsverket"
